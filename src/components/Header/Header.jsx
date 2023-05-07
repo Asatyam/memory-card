2b98fdbf8f8f3973fb32045791af62cc8594042d
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-05-07 22:38:41 +0530

    Create the header for the app

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from './Header.module.css'
+
+
+export default function Header(){
+
+    return(
+        <div className={style.header}>
+            <h1>Memory Card</h1>
+        </div>
+    )
+}
